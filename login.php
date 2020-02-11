d98823db51193ee8b1f9d37c60b10e41bf2fa157
NM198 <34718702+NM198@users.noreply.github.com>
2020-02-11 00:43:46 +0000

    Login design
    Login , registration design change added css and backround image

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet"  href="css/sidebar.css">
+        <link rel="stylesheet"  href="css/login.css">
         <link href="https://fonts.googleapis.com/css?family=Roboto:100&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="js/bootstrap.min.js"></script>
@@ -18,12 +19,15 @@
       
     <div class="header">
       
-      <h2> Log In Here </h2>
+      
       
     </div>
     
     <!-- Log In form --->
-    <form action="login.php" method ="POST">
+    <form id ="loginform" action="login.php" method ="POST">
+      <h2 style="text-align:center;"> Log In Here </h2>
+      <br>
+      <br>
       <div>
         
         <label for ="username">Username: </label>
@@ -37,10 +41,14 @@
         <input type="current-password" name="password_1" required>
         
       </div>
-      
-        <button type="submit">Submit</button>
+        <br>
+        <br>
+        <button class="btn btn-primary btn-lg "type="submit">Sign In</button>
+        <br>
+        <br>
         <p>Not a user? <a href="register.php"> Register Here</a></p>
-        
+        <br>
+        <br>
       </form>
       </div>
       
--- register.php
+++ register.php
@@ -8,6 +8,7 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet"  href="css/sidebar.css">
+        <link rel="stylesheet"  href="css/login.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="js/bootstrap.min.js"></script>
         <script src="js/clock.js"> </script>
@@ -19,13 +20,15 @@
       
     <div class="header">
       
-      <h2> Register Here</h2>
+     
       
-    </div>
+  
     
     <!--Registration Form  --->
-    <form action="register.php" method ="POST">
-      
+    <form id="registerform" action="register.php" method ="POST">
+       <h2 style="text-align:center;"> Register Here</h2>
+       <br>
+       <br>
       <?php include('errors.php')?>
       <div>
         
@@ -33,33 +36,37 @@
         <input type="text" name="username" required>
         
       </div>
-      
+      <br>
       <div>
         
-        <label for="email">Email:</label>
+        <label for="email">Email: </label>
         <input type="email" name="email" required>
-        
+       
       </div>
-      
+      <br>
       <div>
         
-        <label for="password">Password:</label>
+        <label for="password">Password: </label>
         <input type="new-password" name="password_1" required>
         
       </div>
-      
+      <br>
       <div>
         
-        <label for="password">Password:</label>
+        <label for="password">Password: </label>
         <input type="new-password" name="password_2" required>
         
       </div>
-      
-        <button type="submit">Submit </button>
+      <br>
+      <br>
+        <button class="btn btn-primary btn-lg" type="user_login" >Submit </button>
+       <br>
+       <br> 
         <p>Already a user? <a href="login.php"> Log in</a></p>
         
     </form>
     
+    </div>
     </div>
     
 </body>
